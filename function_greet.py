22de0786a926cfa86e333e161e8c6ea65e32f308
richarde90 <richardelms@live.com>
2023-08-12 19:02:37 +0100

    New public python_dev repo for python and ML learning.

---
+++ function_greet.py
@@ -0,0 +1,6 @@
+# Example of building a function, defines greet where the parameter is name and outputs an f string with the greeting and dynamic name. 
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("Alice")
