43aacb46078ee0d552cdcadde091d2ac6f7c01ac
richarde90 <richardelms@live.com>
2023-08-13 23:38:20 +0100

    file_handling modification

--- file_handling_txt.py
+++ file_handling_txt.py
@@ -2,3 +2,10 @@ file = open("example.txt","r") # R is read mode
 file_output = file.read()
 print (file_output)
 file.close()
+
+## Alternate method with implied close, with is also beneficial to generally close other resources too. Network, DB connections, files etc.
+
+with open("example.txt", "r") as file:
+    file_output_with = file.read()
+    print(file_output_with)
+    # File automatically closed. 
